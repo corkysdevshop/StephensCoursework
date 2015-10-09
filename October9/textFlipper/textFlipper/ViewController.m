f3e895f01b1fd14aa67814afb2400bcab6921e04
Stephen Printup <stephen.printup@gmail.com>
2015-10-09 14:22:04 -0700

    color text app

--- October9/textFlipper/textFlipper/ViewController.m
+++ October9/textFlipper/textFlipper/ViewController.m
@@ -9,33 +9,62 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    NSMutableArray *colors;
+}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    colors = [[NSMutableArray alloc]init];
+    [colors addObject:[UIColor redColor]];
+    [colors addObject:[UIColor orangeColor]];
+    [colors addObject:[UIColor yellowColor]];
+    [colors addObject:[UIColor greenColor]];
+    [colors addObject:[UIColor blueColor]];
+    [colors addObject:[UIColor purpleColor]];
 }
--(BOOL)textFieldShouldReturn:(UITextField *)textField
+-(BOOL)textFieldShouldReturn:(UITextField *)textField //Return
 {
     if (textField == self.textField1)
     {
     [self.textField2 becomeFirstResponder];
-        [self.view setBackgroundColor:[UIColor redColor]];
     }
     else if (textField == self.textField2)
     {
     [self.textField1 becomeFirstResponder];
-    [self.view setBackgroundColor:[UIColor greenColor]];
     }
+    [self changeColor];
     return  YES;
 }
--
 
+-(BOOL)textFieldShouldBeginEditing:(UITextField *)textField //Begin Editing
+{
+    if (textField == self.textField1)
+    {
+        [self.textField1 isFirstResponder];
+    }
+    else
+    {
+        [self.textField2 isFirstResponder];
+    }
+    [self changeColor];
+    return YES;
+}
 
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
+{
+    [self changeColor];
 
+    return YES;
+}
 
-
-
+-(void)changeColor
+{
+    int variable1 = arc4random() % 6;
+    
+    [self.view setBackgroundColor: colors[variable1]];
+}
 @end
